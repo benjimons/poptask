fb8ce47520d48fd24a1fd0c7638958739443c64d
Ben McDowall <ben.mcdowall@gmail.com>
2016-08-21 20:09:57 +1200

    Update task_populator.php

--- task_populator.php
+++ task_populator.php
@@ -1,13 +1,12 @@
 <?php
+//ByPass authentication
 $bypass=true;
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
+//Connect to the database
 include_once('db_connect.php');
 $mysqlim=connectdb(true);
 $mysqlim2=connectdb(true);
 //Send the mail for stuff coming due that hasnt been completed yet
 $mailstmt = $mysqlim->prepare("SELECT notify.NotifyID, tasks_working.Scheduled, tasks_master.Title, users.Email, CURRENT_TIMESTAMP from tasks_working LEFT JOIN tasks_master on tasks_master.TaskID=tasks_working.TaskID LEFT JOIN notify ON notify.TaskID=tasks_master.TaskID LEFT JOIN users on notify.UserID=users.UserID WHERE tasks_working.Scheduled<DATE_ADD(CURRENT_TIMESTAMP, INTERVAL 30 MINUTE) AND tasks_master.Active='Y' AND notify.LastSent<>DATE(NOW()) AND users.Active='Y' AND (tasks_working.CompletedBy='' OR tasks_working.CompletedBy IS NULL OR tasks_working.CompletedBy=0)");
-
 $mailstmt->execute();
 $mailstmt->bind_result($rmid, $rmscheduled, $rmtitle, $rmemail, $thistime);
 
